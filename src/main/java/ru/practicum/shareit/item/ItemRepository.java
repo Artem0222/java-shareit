cf2421d08e132e2bd70305712c88dbce48b237d4
Artem0222 <arzerby11@github.com>
2026-08-19 20:55:16 +0300

    no where in hql

--- src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -11,7 +11,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     @Query("select i from Item i " +
             "where i.available = true AND (" +
-            "where upper(i.name) like upper(concat('%', ?1, '%')) " +
-            "or upper(i.description) like upper(concat('%', ?1, '%'))")
+            "upper(i.name) like upper(concat('%', ?1, '%')) " +
+            "OR upper(i.description) like upper(concat('%', ?1, '%')))")
     List<Item> search(String text);
 }
